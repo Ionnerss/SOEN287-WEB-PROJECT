60c4e4c331f6328f7092cdf55b4a4ee311b1b2a8
Catalin-Ion Besleaga <c_beslea@live.concordia.ca>
2026-03-23 15:05:49 -0400

    comments

--- client/js/auth.js
+++ client/js/auth.js
@@ -1,5 +1,5 @@
-// Temporary login for frontend
 
+//LOGIN SECTION OF AUTHENTICATION
 document.addEventListener('DOMContentLoaded', () => {
   const loginForm = document.getElementById('loginForm');
   if (!loginForm) return;
@@ -16,6 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const adminEmail = 'admin@domain.com';
     const correctPassword = '1234';
 
+
+    //Same logic we will follow for the following 2 if statements. Have to figure out how to retrieve data now.
     // Student login
     if (email === studentEmail && password === correctPassword) {
       localStorage.setItem('userRole', 'student');
@@ -30,8 +32,19 @@ document.addEventListener('DOMContentLoaded', () => {
       return;
     }
 
+    /*
+    * Here we have to switch it up a bit to inform the user which is wrong.
+    * If the email is wrong, we say invalid email adress 
+    * if something wrong with password we say invalid credentials
+    * !!! We could also setup a counter system, that way they have a limited amount of tries of login fails
+    *   - Set up counter
+    *   - Set up some sort of cooldown timer
+    */
     // Invalid login
     errorBox.hidden = false;
     errorBox.textContent = 'Invalid email or password.';
   });
 });
+
+
+//SIGNUP SECTION OF AUTHENTICATION
