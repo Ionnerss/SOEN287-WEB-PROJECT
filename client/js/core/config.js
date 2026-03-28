9e4a0aeb3fc482526ef14d5902dcb5215174ef4a
Catalin-Ion Besleaga <c_beslea@live.concordia.ca>
2026-03-28 19:07:54 -0400

    Update config.js
    - added objects which will simplify code later on.

--- client/js/core/config.js
+++ client/js/core/config.js
@@ -1,8 +1,36 @@
 window.APP_CONFIG = {
-    appName: "Smart Course Companion"
-};
+    appName: "Smart Course Companion",
+    
+    api: {
+        baseUrl: "http://localhost:3000",
+        basePath: "/api"
+    },
 
-api: {
-    baseURL: "";
-    basePATH: "";
-}
+    auth: {
+        login: "/auth/login",
+        signup: "/auth/signup",
+        logout: "/auth/logout",
+        twofa: "/auth/twofa",
+        guard: "/auth/guard"
+    },
+
+    //Might be useful later on when we'll know exact final paths
+    // pages: {
+    //     login: "/pages/auth/login.html",
+    //     signup: "/pages/auth/signup.html",
+    //     verify2fa: "/pages/auth/verify-2fa.html",
+    //     studentDashboard: "/pages/student/dashboard.html",
+    //     adminDashboard: "/pages/admin/dashboard.html"
+    // },
+
+    roles: {
+        student: "student",
+        admin: "admin"
+    },
+    
+    //Same here for the final paths
+    // redirects: {
+    //     student: "...", //will build upon as we build
+    //     admin: "..."    //server and make frontend manipulate data
+    // }
+};
