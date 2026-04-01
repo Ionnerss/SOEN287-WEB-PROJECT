69c764dc4003bbd71a15232ad38d00055023ca55
Catalin-Ion Besleaga <c_beslea@live.concordia.ca>
2026-04-01 12:33:01 -0400

    Base URL fixes
    - Figuring out front/back end communication

--- client/js/core/config.js
+++ client/js/core/config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   appName: "Smart Course Companion",
   api: {
-    baseUrl: "http://localhost:3000",
+    baseUrl: "http://localhost:5500",
     basePath: "api",
   },
   auth: {
